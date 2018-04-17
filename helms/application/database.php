e1fe634e4abe9962a6163b1549e1a23fe7159012
Administrator <Administrator@WIN7-1707052206>
2018-04-17 20:10:43 +0800

    wqreq

--- helms/application/database.php
+++ helms/application/database.php
@@ -15,11 +15,11 @@ return [
     // 锟斤拷锟斤拷锟斤拷锟斤拷址
     'hostname'        => '127.0.0.1',
     // 锟斤拷锟捷匡拷锟斤拷
-    'database'        => 'helms',
+    'database'        => 'mydb',
     // 锟矫伙拷锟斤拷
     'username'        => 'root',
     // 锟斤拷锟斤拷
-    'password'        => '',
+    'password'        => '023839',
     // 锟剿匡拷
     'hostport'        => '3306',
     // 锟斤拷锟斤拷dsn
